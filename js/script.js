21ccfea8171b63d408c093118f4d6e1733717caf
dashulya11 <91964833+dashulya11@users.noreply.github.com>
2022-01-05 20:08:11 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const calculator = {
+
+    read() {
+        this.num1 = +prompt('Введите первое число:');
+        if (typeof this.num1 !== Number && isNaN(this.num1)) {
+            alert('first second number is wrong!');
+            this.read();
+        }
+        this.num2 = +prompt('Введите второе число:');
+        if (typeof this.num2 !== Number && isNaN(this.num2)) {
+            alert('second number is wrong!');
+            this.read();
+        }
+        console.log(this);
+    },
+    sum() {
+        return  this.num1 + this.num2;
+    },
+    mul() {
+        return this.num1 * this.num2;
+    }
+}
+
+calculator.read();
+alert(`суммирование: ${calculator.sum()}`);
+alert( `умножение: ${calculator.mul()}`);
